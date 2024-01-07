4f856a297f3c80dba3426246e13bd7dfcbb15398
Felipe Maion <felipe.maion@gmail.com>
2024-01-07 10:34:45 -0300

    Implement calculator

--- Chapter6/calculator.cpp
+++ Chapter6/calculator.cpp
@@ -183,7 +183,8 @@ int main(int argc, char *argv[])
     table["pi"] = 3.1415926535897932385;
     table["e"] = 2.7182818284590452354;
 
-    while (input->good() && !input->eof())
+    // while (input->good() && !input->eof())
+    while (*input)
     {
         cout << ">";
         get_token();
